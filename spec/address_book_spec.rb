69fe61dcb8788f3a69c40af3f5856dcf59200db2
nahome <nahom.teshome@yahoo.com>
2018-02-22 14:29:08 -0800

    models assign

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,31 @@ require_relative '../models/address_book'
          expect(new_entry.email).to eq('augusta.king@lovelace.com')
        end
      end
+
+     describe "#remove_entry" do
+         it "removes only one entry to the address book" do
+           book = AddressBook.new
+           book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+           name = 'Leo Messi'
+           phone_number = '011.012.1815'
+           email = 'kingLeo.goat@barca.com'
+           book.add_entry(name,phone_number,email_address)
+
+           expect(book.entries.size).to eq(2)
+           book.remove_entry(name,phone_number,email_address)
+           expect(book.entries.size).to eq(1)
+           expect(book.entries.first.name).to eq('Ada Lovelace')
+         end
+
+         it "adds the correct information to entries" do
+           book = AddressBook.new
+           book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+           new_entry = book.entries[0]
+
+           expect(new_entry.name).to eq('Ada Lovelace')
+           expect(new_entry.phone_number).to eq('010.012.1815')
+           expect(new_entry.email).to eq('augusta.king@lovelace.com')
+         end
+       end
    end
